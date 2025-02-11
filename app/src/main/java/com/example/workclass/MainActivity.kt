051a86b4a0c0f41a0419d87730fecb96d9b4f55d
Humberto-Martin-1999 <c17150832@aguascalientes.tecnm.mx>
2025-02-11 10:56:41 -0600

    Use of Modifiers and Compose

--- app/src/main/java/com/example/workclass/MainActivity.kt
+++ app/src/main/java/com/example/workclass/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.workclass
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.workclass.ui.theme.WorkClassTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,43 +28,93 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             WorkClassTheme {
-               Row(){
-                   Column(){
-                       Text(text = "Hello World!")
-                       Text(text = "Welcome to Android this is yor first APP")
-                   }
-                   Column(){
-                       Text(text = "Hello World!")
-                       Text(text = "Welcome to Android this is yor first APP")
-                   }
-                   Column(){
-                       Text(text = "Hello World!")
-                       Text(text = "Welcome to Android this is yor first APP")
-                   }
-
+                Column{
+                    Column {
+                        TextComposable("Humberto")
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
+                    }
+                    Row {
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
+                    }
+                    Column {
+                        ModifierExampleTwo()
+                    }
                 }
-               /* Row(){
-                    Text(text = "Hello World!")
-                    Text(text = "Welcome to Android this is yor first APP")
-                }*/
-                // sirve para poder poner poner un elemento delante del otoro pudiendo definir en donde queda
-              /*  Box(){
-                    Text(text = "Hello World!")
-                    Text(text = "Welcome to Android this is yor first APP")
-                }*/
 
-                // A surface container using the 'background' color from the theme
-               /* Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   Greeting(
+                /*
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
-                */
+
+                 */
             }
         }
     }
-}
+
+
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun TextComposable(name:String = "Humberto"){
+        Text("Bienvenido")
+        Text(name)
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExampleOne(){
+        Column (
+            modifier = Modifier
+                .padding(24.dp,10.dp,5.dp,20.dp)
+
+        ){
+            Text("My name is")
+        }
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExampleTwo(){
+        Column (
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+                .clickable(onClick={})
+        ){
+            Text("My name is")
+        }
+    }
+   /* fun clickAction(){
+        println("column clicked")
+    }*/
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExampleThree(){
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(16.dp)
+                .background(Color.Cyan)
+                .border(width = 2.dp, Color.Black)
+                .width(200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextComposable("1.")
+            TextComposable("2")
+            TextComposable("3")
+            TextComposable("4")
+        }
+    }
+
+
+
 /*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -71,8 +127,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    WorkClassTheme {
+    LittleProjectAndroidTheme {
         Greeting("Android")
-    }
-}
-*/
+    }
+}*/
