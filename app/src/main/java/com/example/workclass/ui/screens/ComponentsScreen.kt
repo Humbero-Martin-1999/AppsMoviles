dd793fef0051a62ba2e4c5d5441a18c25e9b0ac1
Humberto-Martin-1999 <c17150832@aguascalientes.tecnm.mx>
2025-03-03 19:19:33 -0600

    Added a line and progress circle feature

--- app/src/main/java/com/example/workclass/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/workclass/ui/screens/ComponentsScreen.kt
@@ -3,15 +3,19 @@ package com.example.workclass.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
@@ -20,13 +24,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 
 @Composable
 fun ComponentsScreen(navController: NavHostController) {
     //Buttons()
-    Floatingbuttons()
+    //Floatingbuttons()
+    Progress()
 
 
 }
@@ -65,7 +71,7 @@ fun Buttons() {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun Floatingbuttons() {
     Column(
@@ -88,6 +94,28 @@ fun Floatingbuttons() {
             Icon(Icons.Filled.Add, "Add Button" )
                 Text(text = "Extended Button")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Progress() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(64.dp)
+        )
+
     }
 }
 
