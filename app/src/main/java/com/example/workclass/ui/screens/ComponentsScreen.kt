2799f2098094cac5d33adef80c0a19fd2e23467a
Humberto-Martin-1999 <c17150832@aguascalientes.tecnm.mx>
2025-03-03 19:10:52 -0600

    in this change we implement new kind of floating buttons in a new function.

--- app/src/main/java/com/example/workclass/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/workclass/ui/screens/ComponentsScreen.kt
@@ -3,10 +3,17 @@ package com.example.workclass.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -18,12 +25,13 @@ import androidx.navigation.NavHostController
 
 @Composable
 fun ComponentsScreen(navController: NavHostController) {
-    Buttons()
+    //Buttons()
+    Floatingbuttons()
 
 
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun Buttons() {
     Column(
@@ -54,6 +62,32 @@ fun Buttons() {
 
         }
 
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Floatingbuttons() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+
+    ) {
+        FloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add Button" )
+        }
+        SmallFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add Button" )
+        }
+        LargeFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add Button" )
+        }
+        ExtendedFloatingActionButton(onClick = {}) {
+            Icon(Icons.Filled.Add, "Add Button" )
+                Text(text = "Extended Button")
+        }
     }
 }
 
